b395eaaebff17d919ae77bf444a38285c71f50cd
Theo <theosnyman.dev@gmail.com>
2025-10-11 16:40:40 +0200

    scaffold project

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="temp-landing">
+    <h1>Project Scaffolded 😁</h1>
+  </div>
+</template>
+
+<script setup lang="ts"></script>
+
+<style scoped>
+.temp-landing {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  min-height: 97dvh; /** > 97 creates scrollbars */
+}
+</style>
