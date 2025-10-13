d15bdb513fd9306d04c29bd62d154f6d36050cde
Theo <theosnyman.dev@gmail.com>
2025-10-13 23:00:39 +0200

    revise signup flow

--- src/components/SignupFlow.vue
+++ src/components/SignupFlow.vue
@@ -1,10 +1,12 @@
 <template>
+  Signup Flow
+
   <!--
   stepper
 
-  step 1: first name, last name, email, password, confirm password
-  step 2: create practice or join existing practice
-  step 3a: create practice - name, address, phone, email, practice type, website
-  step 3b: join existing practice - still need to figure out what todo here
+  step 1: first name, last name, email, phone, password, confirm password
+  step 2: create or join existing practice
+  step 3a: create - name, email, phone, type
+  step 3b: join - ???
   -->
 </template>
